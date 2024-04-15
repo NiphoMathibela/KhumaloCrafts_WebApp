7ec4d526c6abed58e6d0b2194313060589dceb9f
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2024-04-15 14:02:48 +0200

    Added product model

---
+++ KhumaloCrafts.WebApp/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace KhumaloCrafts.WebApp.Models
+{
+    public class Product
+    {
+        public string? ProductName { get; set; }
+
+        public string? ProductDescription { get; set; }
+
+        public string? ProductCategory { get; set; }
+
+        public string? ProductPrice { get; set; }
+
+        public string? Img { get; set; }
+    }
+}
---
+++ KhumaloCrafts.WebApp/Pages/Product Pages/AddProduct.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KhumaloCrafts.WebApp.Pages.Product_Pages
+{
+    public class AddProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
